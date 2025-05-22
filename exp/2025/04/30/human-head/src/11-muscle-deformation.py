56b3787ec18174b80ae6a2df8a42a3718e14c6a9
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-22 16:03:18 +0800

    chore(cherries): auto commit (on run end)
    🏃 View run 2025/04/30/human-head at: https://mlflow.liblaf.me:38161/#/experiments/3/runs/22eee62fb0014f27bef03cb4db41b009
    🧪 View experiment 3 at: https://mlflow.liblaf.me:38161/#/experiments/3

--- exp/2025/04/30/human-head/src/11-muscle-deformation.py
+++ exp/2025/04/30/human-head/src/11-muscle-deformation.py
@@ -1,19 +1,30 @@
 from pathlib import Path
 
+import jax
+import jax.numpy as jnp
 import pyvista as pv
+from jaxtyping import Float
 
+import liblaf.apple as apple  # noqa: PLR0402
 from liblaf import cherries, melon
 
 
 class Config(cherries.BaseConfig):
     solution: Path = cherries.data("solution-100/solution_000101.vtu")
+    tetmesh: Path = cherries.data("10-tetgen.vtu")
 
 
 def main(cfg: Config) -> None:
     solution: pv.UnstructuredGrid = melon.load_unstructured_grid(cfg.solution)
-    tetmesh: pv.UnstructuredGrid = melon.load_unstructured_grid(
-        cherries.data("10-tetgen.vtu")
-    )
+    tetmesh: pv.UnstructuredGrid = melon.load_unstructured_grid(cfg.tetmesh)
+
+    points: Float[jax.Array, "P 3"] = jnp.asarray(tetmesh.points)
+    cells: Float[jax.Array, "C 4"] = jnp.asarray(tetmesh.cells_dict[pv.CellType.TETRA])
+    dh_dX: Float[jax.Array, "*C 4 3"] = apple.jax.elem.tetra.dh_dX(points[cells])
+    u: Float[jax.Array, "*C 4 3"] = jnp.asarray(solution.point_data["solution"])
+    F: Float[jax.Array, "*C 3 3"] = apple.jax.elem.tetra.deformation_gradient(u, dh_dX)
+
+    ic(F)
 
 
 if __name__ == "__main__":

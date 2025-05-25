7c3d52ad8626a890abf605f823e9b9189cc420ce
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-25 21:24:51 +0800

    fix(benches): check CUDA availability before setting mempool threshold
    Prevents potential errors when running benchmarks on non-CUDA devices

--- benches/conftest.py
+++ benches/conftest.py
@@ -3,4 +3,5 @@ import warp as wp
 
 
 def pytest_configure(config: pytest.Config) -> None:  # noqa: ARG001
-    wp.set_mempool_release_threshold(wp.get_preferred_device(), 0.25)
+    if wp.is_cuda_available():
+        wp.set_mempool_release_threshold(wp.get_preferred_device(), 0.25)

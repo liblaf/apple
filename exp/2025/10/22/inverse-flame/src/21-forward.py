6d4c5ed882a72c166a82c67403940c04a2ddee03
liblaf <30631553+liblaf@users.noreply.github.com>
2025-12-22 20:14:50 +0800

    chore(cherries): 2025/10/22/inverse-flame/src/21-forward.py
    url: https://www.comet.com/liblaf/apple/1e0b25e26f95491f879728a10c487e34
    exp_dir: exp/2025/10/22/inverse-flame
    cwd: exp/2025/10/22/inverse-flame
    cmd: python src/21-forward.py
    params:
      curr_step: 1000
    temps:
    - exp/2025/10/22/inverse-flame/temp/20-inverse-adam-123k.vtu.d/20-inverse-adam-123k_000087.vtu
    - exp/2025/10/22/inverse-flame/temp/21-forward.vtu

--- exp/2025/10/22/inverse-flame/src/21-forward.py
+++ exp/2025/10/22/inverse-flame/src/21-forward.py
@@ -89,7 +89,7 @@ class PNCG(OrigPNCG):
             H_diag = state.hess_diag_flat
         P: Vector = jnp.reciprocal(H_diag)
         ic(jnp.min(P), jnp.max(P), jnp.mean(P))
-        # P = jnp.ones_like(P)  # No preconditioning
+        P = jnp.ones_like(P)  # No preconditioning
         beta: Scalar
         p: Vector
         if state.search_direction_flat is None:
@@ -106,10 +106,10 @@ class PNCG(OrigPNCG):
                     "Stagnation detected: gradient norm has not decreased for 20 steps."
                 )
                 beta = jnp.zeros(())
-                H_diag = objective.hess_diag(state.params_flat)
-                H_diag = jnp.where(H_diag <= 0.0, 1.0, H_diag)
-                P = jnp.reciprocal(H_diag)
-                # P = jnp.ones_like(P)  # Reset preconditioner
+                # H_diag = objective.hess_diag(state.params_flat)
+                # H_diag = jnp.where(H_diag <= 0.0, 1.0, H_diag)
+                # P = jnp.reciprocal(H_diag)
+                P = jnp.ones_like(P)  # Reset preconditioner
             else:
                 beta = self._compute_beta(
                     g_prev=state.grad_flat, g=g, p=state.search_direction_flat, P=P

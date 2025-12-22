dc1aae2f1129e7cfe2723aaf34d564862e8acde7
liblaf <30631553+liblaf@users.noreply.github.com>
2025-12-22 20:42:47 +0800

    chore(cherries): 2025/10/22/inverse-flame/src/21-forward.py
    url: https://www.comet.com/liblaf/apple/813ed68024e64d13a7b1b979b9c4f002
    exp_dir: exp/2025/10/22/inverse-flame
    cwd: exp/2025/10/22/inverse-flame
    cmd: python src/21-forward.py
    params:
      curr_step: 6
    temps:
    - exp/2025/10/22/inverse-flame/temp/20-inverse-adam-123k.vtu.d/20-inverse-adam-123k_000087.vtu
    - exp/2025/10/22/inverse-flame/temp/21-forward.vtu

--- exp/2025/10/22/inverse-flame/src/21-forward.py
+++ exp/2025/10/22/inverse-flame/src/21-forward.py
@@ -85,8 +85,8 @@ class PNCG(OrigPNCG):
         H_diag: Vector
         g, H_diag = objective.grad_and_hess_diag(state.params_flat)
         H_diag = jnp.where(H_diag <= 0.0, 1.0, H_diag)
-        # if state.hess_diag_flat is not None:
-        #     H_diag = state.hess_diag_flat
+        if state.hess_diag_flat is not None:
+            H_diag = state.hess_diag_flat
         P: Vector = jnp.reciprocal(H_diag)
         ic(jnp.min(P), jnp.max(P), jnp.mean(P))
         # P = jnp.ones_like(P)  # No preconditioning

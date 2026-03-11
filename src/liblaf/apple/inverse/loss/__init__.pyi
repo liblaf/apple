13584fc32a744963a744dad89a8a4775fb35affb
liblaf <30631553+liblaf@users.noreply.github.com>
2026-03-11 14:55:43 +0800

    chore(cherries): 2026/01/28/smas/src/30-inverse.py
    url: https://www.comet.com/liblaf/apple/19f667ae24654fddafa6f1792bfb37d6
    exp_dir: exp/2026/01/28/smas
    cwd: exp/2026/01/28/smas
    cmd: python ./src/30-inverse.py
    params:
      target: /home/liblaf/github/liblaf/apple/exp/2026/01/28/smas/data/20-forward-whole-smas02-muscle02-act2.vtu
    inputs:
    - exp/2026/01/28/smas/data/20-forward-whole-smas02-muscle02-act2.vtu

--- src/liblaf/apple/inverse/loss/__init__.pyi
+++ src/liblaf/apple/inverse/loss/__init__.pyi
@@ -1,5 +1,6 @@
 from ._base import Loss
 from ._point_to_point import PointToPointLoss
 from ._smooth import SmoothActivationLoss
+from ._uniform import UniformActivationLoss
 
-__all__ = ["Loss", "PointToPointLoss", "SmoothActivationLoss"]
+__all__ = ["Loss", "PointToPointLoss", "SmoothActivationLoss", "UniformActivationLoss"]

e3d87210f0df783f00cd93791e8c5a11d1e96fee
liblaf <30631553+liblaf@users.noreply.github.com>
2026-03-18 01:51:02 +0800

    chore(cherries): 2026/01/28/smas/src/30-inverse.py
    url: https://www.comet.com/liblaf/apple/cf0aed1dc78248728f44d8aff53e4662
    exp_dir: exp/2026/01/28/smas
    cwd: exp/2026/01/28/smas
    cmd: python ./src/30-inverse.py
    params:
      target: /home/liblaf/github/liblaf/apple/exp/2026/01/28/smas/data/20-forward-smas46-muscle46-prestrain.vtu
    inputs:
    - exp/2026/01/28/smas/data/20-forward-smas46-muscle46-prestrain.vtu
    temps:
    - exp/2026/01/28/smas/temp/30-inverse-smas46-muscle46-prestrain.vtu.series

--- test_svd.py
+++ test_svd.py
@@ -1,19 +1,25 @@
-import warp as wp
 import numpy as np
+import warp as wp
 
 wp.init()
+
 
 @wp.func
 def svd_rv(F: wp.mat33):
     U, sigma, V = wp.svd3(F)
     return U @ wp.transpose(V)
 
+
 @wp.kernel
 def test_grad(A: wp.array(dtype=wp.mat33), out: wp.array(dtype=float)):
     R = svd_rv(A[0])
     out[0] = wp.trace(R)
 
-A = wp.array(np.array([[[1.0, 0.1, 0.2], [0.1, 1.0, 0.3], [0.2, 0.3, 1.0]]], dtype=np.float32), requires_grad=True)
+
+A = wp.array(
+    np.array([[[1.0, 0.1, 0.2], [0.1, 1.0, 0.3], [0.2, 0.3, 1.0]]], dtype=np.float32),
+    requires_grad=True,
+)
 out = wp.array(np.zeros(1, dtype=np.float32), requires_grad=True)
 
 with wp.Tape() as tape:
